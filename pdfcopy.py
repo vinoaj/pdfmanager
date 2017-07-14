d8888eee711c36c3f42d1f79349c749e83b18a4e
vinoaj <vinoaj@gmail.com>
2017-07-14 14:15:57 +0800

    Initial commit

---
+++ pdfcopy.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+from PyPDF2 import PdfFileReader, PdfFileWriter
+
+pdf_input = None
+pdf_output = None
+
+
+def open_file(path, file_password):
+    pdf_input = PdfFileReader(open(path, "rb"))
+
+    if pdf_input.isEncrypted is True:
+        pdf_input.decrypt(file_password)
+
+    return pdf_input
+
+
+def make_copy(pdf_file, pdf_path):
+    pdf_output = PdfFileWriter()
+
+    file_name, file_extension = os.path.splitext(pdf_path)
+    file_name_new = file_name + '-new'
+    path_new = file_name_new + file_extension
+
+    for page in pdf_file.pages:
+        pdf_output.addPage(page)
+
+    output_stream = open(path_new, "wb")
+    pdf_output.write(output_stream)
+
+    return output_stream
+
+
+if __name__ == "__main__":
+    pdf_path = sys.argv[1]
+    pdf_password = None
+
+    if len(sys.argv) > 2:
+        pdf_password = sys.argv[2]
+
+    f = open_file(pdf_path, pdf_password)
+    fn = make_copy(f, pdf_path)
